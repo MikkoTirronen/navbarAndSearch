5a1db65f4662bb08bab5b9a014f44e2fe01ae58a
Mikko Tirronen <mikkoatirronen@gmail.com>
2022-11-20 22:30:29 +0100

    added home page componenet

---
+++ src/pages/HomePageComponent.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import HeaderComponent from "../components/header/HeaderComponent";
+import NavbarComponent from "../components/navbar/NavbarComponent";
+
+export default function HomePageComponent() {
+  return (
+    <div>
+      <HeaderComponent />
+      <NavbarComponent />
+    </div>
+  );
+}
